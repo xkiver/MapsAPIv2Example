b61a86f91f5f000c3c82e7b827bf51173983e41f
xkiver <xkiver@gmail.com>
2013-11-16 01:33:05 -0300

    - Primer commit

---
+++ gen/cl/telematica/mapsv2example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cl.telematica.mapsv2example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cl/telematica/mapsv2example/interfaces/LocationListenerHandler.java
@@ -0,0 +1,9 @@
+package cl.telematica.mapsv2example.interfaces;
+
+import android.location.Location;
+
+public interface LocationListenerHandler {
+	
+	public void OnLocationReceived(Location loc);
+
+}
